e310d9f7763b461c12fdd82fa55e888c8c31e6e1
SevaStopAll <sevastopallrh@yandex.ru>
2023-03-31 13:16:19 +0400

    2. Сканирование проекта [#1070]

--- src/main/java/ru/job4j/di/Main.java
+++ src/main/java/ru/job4j/di/Main.java
@@ -5,9 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
-        context.register(Store.class);
-        context.register(StartUI.class);
-        context.register(ConsoleInput.class);
+        context.scan("ru.job4j.di");
         context.refresh();
         ConsoleInput input = context.getBean(ConsoleInput.class);
         input.askStr("What is your name?");
--- src/main/java/ru/job4j/di/StartUI.java
+++ src/main/java/ru/job4j/di/StartUI.java
@@ -1,5 +1,7 @@
 package ru.job4j.di;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StartUI {
 
     private Store store;
