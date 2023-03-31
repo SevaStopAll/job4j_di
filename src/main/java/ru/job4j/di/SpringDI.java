929a82831f4a4dd918e36b85d90ef4ad48569d63
SevaStopAll <sevastopallrh@yandex.ru>
2023-03-31 13:00:27 +0400

    1. Spring DI. Запуск [#2091]

--- src/main/java/ru/job4j/di/SpringDI.java
+++ src/main/java/ru/job4j/di/SpringDI.java
@@ -2,14 +2,15 @@ package ru.job4j.di;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class Main {
+public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(Store.class);
         context.register(StartUI.class);
-        context.register(ConsoleInput.class);
         context.refresh();
-        ConsoleInput input = context.getBean(ConsoleInput.class);
-        input.askStr("What is your name?");
+        StartUI ui = context.getBean(StartUI.class);
+        ui.add("Petr Arsentev");
+        ui.add("Ivan ivanov");
+        ui.print();
     }
 }
